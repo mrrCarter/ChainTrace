47cca82b065c448c7fcadf94f9b2eda41683e8f8
Kürşat Alyamaç <rkalyamac@gmail.com>
2023-12-09 09:32:01 -0500

    Implemented baud rate generator for UART connection

---
+++ verilog_code/baud_rate_generator.v
@@ -0,0 +1,29 @@
+module baud_rate_generator
+    #(              // 9600 baud
+        parameter   N = 10,     // number of counter bits
+                    M = 651     // counter limit value
+    )
+    (
+        input clk_100MHz,       // artix7-100T clock
+        input reset,            // reset
+        output tick             // sample tick
+    );
+    
+    // Counter Register
+    reg [N-1:0] counter;        // counter value
+    wire [N-1:0] next;          // next counter value
+    
+    // Register Logic
+    always @(posedge clk_100MHz, posedge reset)
+        if(reset)
+            counter <= 0;
+        else
+            counter <= next;
+            
+    // Next Counter Value Logic
+    assign next = (counter == (M-1)) ? 0 : counter + 1;
+    
+    // Output Logic
+    assign tick = (counter == (M-1)) ? 1'b1 : 1'b0;
+       
+endmodule
